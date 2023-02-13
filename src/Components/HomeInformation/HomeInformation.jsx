0842be52cc7f4add0fdc00fbb462d4e9f4ddc4ab
Abdul <abdullahmd326@gmail.com>
2023-02-13 14:33:09 -0500

    Adding Link from react-router where appropriate

--- src/Components/HomeInformation/HomeInformation.jsx
+++ src/Components/HomeInformation/HomeInformation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HorizontalRule from '../HorizontalRule/HorizontalRule.jsx';
 import '../../css/homeinformation.css';
+import { Link } from 'react-router-dom';
 
 const HomeInformation = () => {
 
@@ -8,35 +9,35 @@ const HomeInformation = () => {
             <section id="mid-section">
                 <div className="mid-upper-row row">
                     <div className="resume-row col-lg-4">
-                        <a className="icon-address" href="/resume">
+                        <Link className="icon-address" to="/resume">
                             <i className="fas fa-file-alt fa-6x"></i>
                             <h2 className="mid-heading">Resume</h2>
                             <p className="mid-paragraph">A comprehensive overview about me just one click away!</p>
-                        </a>
+                        </Link>
                     </div>
                 </div>
                 <HorizontalRule />
                 <div className="mid-row row">
                     <div className="mid-row-section border-r col-lg-4 col-md-4 col-sm-4">
-                        <a className="icon-address" href="/about">
+                        <Link className="icon-address" to="/about">
                             <i className="fas fa-address-card fa-6x"></i> 
                             <h2 className="mid-heading">About</h2>
                             <p className="mid-paragraph">You can learn more about me and where/why/what I pursued as education.</p>
-                        </a>
+                        </Link>
                     </div>
                     <div className="mid-row-section border-r col-lg-4 col-md-4 col-sm-4">
-                        <a className="icon-address" href="/skills">
+                        <Link className="icon-address" to="/skills">
                             <i className="fas fa-laptop-code fa-6x"></i>  
                             <h2 className="mid-heading">Skills</h2>
                             <p className="mid-paragraph">Information pertaining to my skill set and technology stack.</p>
-                        </a>
+                        </Link>
                     </div>
                     <div className="mid-row-section col-lg-4 col-md-4 col-sm-4">
-                        <a className="icon-address" href="/leisure">
+                        <Link className="icon-address" to="/leisure">
                             <i className="fas fa-chess-knight fa-6x"></i> 
                             <h2 className="mid-heading">Leisure</h2>
                             <p className="mid-paragraph">I love playing sports during free time. As a developer, it is important to relax and find time for yourself!</p>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </section>
