d1e28e4cad317a869622a40913f7ea466930ddbb
Abdul <abdullahmd326@gmail.com>
2023-02-13 20:54:34 -0500

    Refactoring final for remaining pages, portfolio page complete, link later

--- src/Components/PortfolioPage/portfoliopage.jsx
+++ src/Components/PortfolioPage/portfoliopage.jsx
@@ -7,10 +7,10 @@ const PortfolioPage = () => {
                 <h1 className="programming-heading">Portfolio</h1>
                 <p className="programming-paragraph">Here, on this page, you will find several recent/past projects I had been working on, their brief descriptions and links to their actual place on the interwebs. </p>
                 <p className="programming-paragraph">Some of these are static, others have a full fledged back end working behind the scenes. Nevertheless, they're all deployed to the cloud via AWS.</p>
-                <p className="programming-paragraph">The full source code, building the project from the ground up, will be provided as well. You can also visit my Github profile to see all public repositories aside from the ones listed here under my name. </p>
-                <p className="programming-paragraph">The link to my github profile is: <b><i><u><a href="https://github.com/CodingAbdullah" target="_blank" rel="noreferrer">Github Profile</a></u></i></b></p>
+                <p className="programming-paragraph">The full source code, building the project from the ground up, can be found on my Github profile. </p>
+                <p className="programming-paragraph">The link to my Github profile is: <b><i><u><a style={{ color: 'green' }} href="https://github.com/CodingAbdullah" target="_blank" rel="noreferrer">Github Profile</a></u></i></b></p>
                 <p className="programming-paragraph">You can also visit this on any other page by clicking on the Github icon under the footer section of the pages.</p>
-                <p className="programming-paragraph">More coming soon...</p>
+                <p className="programming-paragraph">Portfolio link: <b><i><u><a style={{ color: 'green' }} href="">Portfolio Link</a></u></i></b></p>
         </div>
     )
     // More to be added later...
