abb58e2f0278b4021b61de1dd3e2319e6659fedc
CodingAbdullah <abdullahmd326@gmail.com>
2025-09-01 23:09:24 -0400

    Adding sonner for future integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import ThemeProvider from "@/components/theme-provider"
 import Navigation  from "@/components/navigation"
 import Footer from "@/components/footer"
 import Script from "next/script"
+import { Toaster } from "sonner"
 import "./globals.css"
 
 const jetBrainsMono = JetBrains_Mono({ subsets: ["latin"] })
@@ -30,6 +31,7 @@ export default function RootLayout({
             data-website-id={process.env.UMAMI_WEBSITE_ID}>
           </Script>
           <Footer />
+          <Toaster position="top-right" duration={5000} />
         </ThemeProvider>
       </body>
     </html>
